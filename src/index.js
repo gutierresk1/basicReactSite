c9be1a5aae9074fbb4c36bac7591aebf67119613
gutierresk1 <43971263+gutierresk1@users.noreply.github.com>
2020-12-14 15:50:06 -0500

    index.js was changed again. To show more usage of JSX

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@
 
 //Create a react component. 
 const App = () => {
-    return <div>Hi there!</div>
+    const buttonText= 'Click me!';
+
+    return (
+    <div>
+        <label className="label" htmlFor="name" >Enter Name: </label>
+        <input id="name" type="text" />
+        <button style={{backgroundColor:'blue', color:'white'}} >
+            {buttonText}
+        </button>
+    </div>
+    );
 };
 
 //Take the react component and show it on the screen. 
